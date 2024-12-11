213c129ec1083b485864afd1203ecff284fb409a
ikbalirawan <iqbalirawan238@gmail.com>
2024-12-11 12:08:34 +0700

    update

--- lib/module/picture/view/detail_picture_view.dart
+++ lib/module/picture/view/detail_picture_view.dart
@@ -11,15 +11,35 @@ class DetailPictureView extends StatelessWidget {
     controller.fetchPictureDetail(id);
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Detail Photo")),
+      appBar: AppBar(title: const Text("Detail Foto")),
       body: Obx(() {
-        if (controller.isLoading.value) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        var detail = controller.pictureDetail;
-        return DetailContentWidget(
-          imageUrl: detail['download_url'],
-          author: detail['author'],
+        // Memastikan widget hanya dibangun ketika status loading berubah
+        return TweenAnimationBuilder(
+          tween: Tween<Alignment>(
+            begin: Alignment.topLeft,
+            end: controller.isLoading.value ? Alignment.bottomRight : Alignment.topRight, // Posisi gradient berubah tergantung status loading
+          ),
+          duration: const Duration(seconds: 5),
+          builder: (context, Alignment alignment, child) {
+            return Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFFFF5722), // Warna pertama
+                    Color(0xFFFFC107), // Warna kedua
+                  ],
+                  begin: alignment, // Gradient bergerak berdasarkan posisi
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              child: controller.isLoading.value
+                  ? const Center(child: CircularProgressIndicator())
+                  : DetailContentWidget(
+                      imageUrl: controller.pictureDetail['download_url'],
+                      author: controller.pictureDetail['author'],
+                    ),
+            );
+          },
         );
       }),
     );
--- lib/module/picture/view/list_picture_view.dart
+++ lib/module/picture/view/list_picture_view.dart
@@ -8,18 +8,62 @@ class ListPictureView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("List Photo")),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(56.0), // Tinggi app bar
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFFFF5722), // Warna pertama
+                Color(0xFFFFC107), // Warna kedua
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: AppBar(
+            title: const Text("List Foto"),
+            backgroundColor: Colors.transparent, // Membuat background app bar transparan
+            elevation: 0, // Menghilangkan shadow pada app bar
+          ),
+        ),
+      ),
       body: Obx(() {
-        if (controller.isLoading.value) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        return ListView.builder(
-          itemCount: controller.pictures.length,
-          itemBuilder: (context, index) {
-            var picture = controller.pictures[index];
-            return ListContentWidget(
-              picture: picture,
-              onTap: () => Get.toNamed(Routes.DETAIL, arguments: picture['id']),
+        // Memastikan widget hanya dibangun ketika status loading berubah
+        return TweenAnimationBuilder(
+          tween: Tween<Alignment>(
+            begin: Alignment.topLeft,
+            end: controller.isLoading.value ? Alignment.bottomRight : Alignment.topRight, // Posisi gradient berubah tergantung status loading
+          ),
+          duration: const Duration(seconds: 5),
+          builder: (context, Alignment alignment, child) {
+            return Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFFFF5722), // Warna pertama
+                    Color(0xFFFFC107), // Warna kedua
+                  ],
+                  begin: alignment, // Gradient bergerak berdasarkan posisi
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              child: Obx(() {
+                if (controller.isLoading.value) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return ListView.builder(
+                  itemCount: controller.pictures.length,
+                  itemBuilder: (context, index) {
+                    var picture = controller.pictures[index];
+                    return ListContentWidget(
+                      picture: picture,
+                      onTap: () =>
+                          Get.toNamed(Routes.DETAIL, arguments: picture['id']),
+                    );
+                  },
+                );
+              }),
             );
           },
         );
